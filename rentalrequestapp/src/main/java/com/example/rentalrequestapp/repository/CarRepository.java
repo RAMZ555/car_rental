a6fe1dbb075c67a03a2fd5d632539dfd61fda471
RAMZ555 <sriramsubramaniyan555@gmail.com>
2025-08-01 01:24:49 +0530

    .

--- rentalrequestapp/src/main/java/com/example/rentalrequestapp/repository/CarRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.rentalrequestapp.repository;
-
-
-
-import com.example.rentalrequestapp.model.Car;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CarRepository extends JpaRepository<Car, Long> {
-}
